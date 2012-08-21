1ad9cfa9c148dc716d3fcf47599ce8ffc2e9bfc0
Karsten Schmidt <k@postspectacular.com>
2012-08-21 01:51:18 +0100

    setting version to 0.1.0 (release), adding description and switching toxiclibs dep to 0.2.1, updating pom

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject com.postspectacular/joglapp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://hg.postspectacular.com/joglui"
+(defproject com.postspectacular/joglapp "0.1.0"
+  :description "Basic app skeleton & utilities for rapid prototyping w/ Clojure and JOGL2"
+  :url "http://hg.postspectacular.com/joglapp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.jogamp/gluegen-rt "20120617"]
                  [com.jogamp/jogl2 "20120617"]
-                 [org.toxiclibs/toxiclibs-clj "0.2.0-SNAPSHOT"]]
+                 [org.toxiclibs/toxiclibs-clj "0.2.1"]]
   :repositories {"nexus-3rd" {:url "http://50.17.220.26:8089/nexus/content/repositories/thirdparty/" :creds :gpg :snapshots false}
                  "nexus-releases" {:url "http://50.17.220.26:8089/nexus/content/repositories/postspectacular-releases/" :creds :gpg :snapshots false}
                  "nexus-snapshots" {:url "http://50.17.220.26:8089/nexus/content/repositories/postspectacular-snapshots/" :creds :gpg}}
