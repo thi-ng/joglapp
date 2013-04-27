4bd215f6bef94caf3e44e5cdacad1964a96ff771
Karsten Schmidt <k@postspectacular.com>
2013-04-27 01:05:19 +0100

    minor updates to import handling

--- src/joglapp/utils.clj
+++ src/joglapp/utils.clj
@@ -1,16 +1,19 @@
-(ns joglapp.utils)
+(ns joglapp.utils
+  (:import
+   [clojure.lang Reflector]
+   [java.beans PropertyDescriptor]))
 
 (defn select-bean-props
   [obj & props]
-  (let [props (apply hash-set props)
-       c (.getClass ^Object obj)]
-    (reduce (fn [m ^java.beans.PropertyDescriptor pd]
+  (let [props (into #{}  props)
+        c (.getClass ^Object obj)]
+    (reduce (fn [m ^PropertyDescriptor pd]
               (let [name (keyword (.getName pd))
                     method (.getReadMethod pd)]
                 (if (and method (props name) (zero? (alength (.getParameterTypes method))))
-                  (assoc m name (clojure.lang.Reflector/prepRet (.getPropertyType pd) (. method (invoke obj nil))))
+                  (assoc m name (Reflector/prepRet (.getPropertyType pd) (. method (invoke obj nil))))
                   m)))
             {}
             (seq (.. java.beans.Introspector
-                   (getBeanInfo c)
-                   (getPropertyDescriptors))))))
+                     (getBeanInfo c)
+                     (getPropertyDescriptors))))))
